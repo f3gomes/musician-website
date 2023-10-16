552d6825bc13390dc65535b3cb39a478d661e687
fomes <fgomesdeluna@gmail.com>
2023-10-16 11:17:17 -0300

    Global style config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  mode: "jit",
   theme: {
+    container: {
+      padding: {
+        default: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768",
+      lg: "960px",
+      xl: "1310",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        singerOverlay: "url(/assets/hero/singer-overlay.png)",
+        newsletter: "url(/assets/newsletter/bg.png)",
+      },
+      fontFamily: {
+        alexBrush: ["var(--font-alexBrush)", "sans-serif"],
+        montserrat: ["var(--font-montserrat)", "sans-serif"],
+      },
+      colors: {
+        primary: "#06062a",
+        secondary: "#151538",
+        tertiary: "#242445",
+        accent: {
+          default: "#7f1cfc",
+          hover: "#6519c6",
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwind-scrollbar")],
+};
+export default config;
